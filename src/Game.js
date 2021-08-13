755197a0129c1b9aaf3a5ecda871c66186b2df00
Ilgaz Zararsız <ilgazzararsiz@gmail.com>
2021-08-14 01:11:27 +0300

    card distribution logic created.

--- src/Game.js
+++ src/Game.js
@@ -1,47 +1,41 @@
 import React from 'react';
 import Deck from './components/Deck';
+import { shuffle, createCards } from './utils';
+import { RANKS, SLICE_POINT_OF_DECKS, COUNT_OF_DECK_ROWS } from './constants';
 import './game.css';
 
 const Game = () => {
 
-  const cards = [
-    {
-      rank: 'A',
+  const cards = shuffle(createCards(RANKS));
+  //const spareCards = cards.slice(0, SLICE_POINT_OF_DECKS);
+  const gameCards = cards.slice(SLICE_POINT_OF_DECKS, cards.length);
+
+  const decks = [];
+  for (let i = 0; i < COUNT_OF_DECK_ROWS; i++) {
+    decks.push([]);
+  }
+  
+  let i = 0;
+
+  gameCards.forEach(gameCard => {
+    decks[i % 10].push({
+      rank: gameCard,
       flipped: true
-    },
-    {
-      rank: '5',
-      flipped: true
-    },
-    {
-      rank: '9',
-      flipped: true
-    },
-    {
-      rank: '9',
-      flipped: true
-    },
-    {
-      rank: '9',
-      flipped: false
-    },
-    {
-      rank: '9',
-      flipped: false
-    },
-    {
-      rank: '9',
-      flipped: false
-    }
-  ];
+    });
+    i++;
+  });
+
+  decks.forEach(deck => {
+    deck[deck.length - 1].flipped = false;
+  });
 
   return <div className="game">
     <div className="game__deck">
-      <Deck cards={ cards } />
-      <Deck cards={ cards } />
-      <Deck cards={ cards } />
-      <Deck cards={ cards } />
-      <Deck cards={ [] }/>
+      {
+        decks.map((deckOfCards, index) => {
+          return <Deck cards={ deckOfCards } key={ index }/>;
+        })
+      }
     </div>
   </div>;
 };
---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+import { COUNT_OF_CARD_RANK } from '../constants';
+
+export const createCards = ranks => {
+  const cards = [];
+  ranks.forEach(rank => {
+    for (let i = 0; i < COUNT_OF_CARD_RANK; i++) {
+      cards.push(rank);
+    }
+  });
+  return cards;
+};
+
+export const shuffle = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
